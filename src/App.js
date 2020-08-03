80a9772f9894788492563c29a6a4aa4106b9092d
Mihail <dzezelomihail@gmail.com>
2020-08-03 08:33:54 +0300

    create new branch

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import {
     BrowserRouter as Router,
     Switch,
     Route,
-    Link
 } from "react-router-dom";
 import {composeWithDevTools} from 'redux-devtools-extension';
 import {createStore, applyMiddleware} from 'redux';
