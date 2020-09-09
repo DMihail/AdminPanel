1fdc763734058a7989a9806ccf42ff5def8bd66e
Admin <dzezelomihail@gmail.com>
2020-09-09 11:02:48 +0300

    add api for get number data, authorization and registration

--- AdminPanelBackEndServer/routes/index.js
+++ AdminPanelBackEndServer/routes/index.js
@@ -1,9 +1,39 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const bodyParser = require('body-parser');
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
+
+// create application/json parser
+const jsonParser = bodyParser.json();
+
+// create application/x-www-form-urlencoded parser
+const urlencodedParser = bodyParser.urlencoded({extended: false});
+
+
+/* GET xml data. */
+router.get('/api/subscribers/:number', function(req, res, next) {
+  console.log(req.params.number);
   res.render('index', { title: 'Express' });
 });
+
+/* GET json data. */
+router.get('/api/subscribers/:number', function(req, res, next) {
+  console.log(req.params.number);
+  res.render('index', { title: 'Express' });
+});
+
+
+/* POST Authorization. */
+router.post('/authorization', jsonParser, function(req, res, next) {
+  console.log(req.body)
+  res.render('index', { title: 'Express' });
+});
+
+/* POST Registration. */
+router.post('/registration', jsonParser, function(req, res, next) {
+  console.log(req.body)
+  res.render('index', { title: 'Express' });
+});
+
 
 module.exports = router;
