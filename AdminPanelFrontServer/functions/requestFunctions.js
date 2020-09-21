2ee64b3250127afd7358ff856d93d4e65480a97f
Admin <dzezelomihail@gmail.com>
2020-09-21 23:59:25 +0300

    add database functions

--- AdminPanelFrontServer/functions/requestFunctions.js
+++ AdminPanelFrontServer/functions/requestFunctions.js
@@ -4,13 +4,9 @@ const axios = require('axios').default;
 const authorization = async (userData) => {
     console.log(userData)
     try {
-        const response = await axios.get('http://localhost:5000/api/front/token', {
+        return await axios.get('http://localhost:5000/api/front/token', {
             params: userData
         });
-        if (response.status === 200) {
-            console.log(response)
-            // res.send({status: true});
-        }
     } catch (error) {
         console.error(error);
     }
@@ -18,7 +14,7 @@ const authorization = async (userData) => {
 
 const getStatusNumber = async (number) => {
     try {
-        const response = await axios.get(`http://localhost:5000/api/subscribers/` + number);
+      return  await axios.get(`http://localhost:5000/api/subscribers/` + number);
     } catch (error) {
         console.error(error);
     }
