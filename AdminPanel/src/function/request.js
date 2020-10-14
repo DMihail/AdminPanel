e2ca22f50c2d3028e83837665667047f638f3850
Admin <dzezelomihail@gmail.com>
2020-10-14 14:11:50 +0300

    fix session

--- AdminPanel/src/function/request.js
+++ AdminPanel/src/function/request.js
@@ -1,14 +1,16 @@
 import axios from 'axios';
 
+const url = "http://localhost:8000";
+
 export const signIn = async (login, password) => {
-    return await axios.post('http://localhost:8000/authorization', {
+    return await axios.post('/authorization', {
         login,
         password
     });
 }
 
 export const checkNumber = async (number) => {
-    return await axios.post('http://localhost:8000/checkNumber', {
+    return await axios.post('/checkNumber', {
         number,
     });
 }
--- AdminPanelFrontServer/app.js
+++ AdminPanelFrontServer/app.js
@@ -5,11 +5,15 @@ const express = require('express'),
     request = require('./functions/requestFunctions'),
    jsonParser = bodyParser.json();
 
+app.use(session({ secret: 'keyboard cat', cookie: { maxAge: 60000 }}))
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static('./build'));
-app.use(session({ secret: 'keyboard cat', cookie: { maxAge: 1500 }}))
 
+app.get('/', function(req, res, next) {
+    res.sendFile(__dirname + "/build/index.html");
+})
 
 app.post('/authorization', jsonParser, async (req, res, next) => {
     const resp = await request.authorization(req.body);
@@ -26,11 +30,11 @@ app.post('/authorization', jsonParser, async (req, res, next) => {
 app.post('/checkNumber', jsonParser, async (req, res, next) => {
     console.log(req.body)
     console.log(req.session)
-    // if (req.session.tocken) {
+    if (req.session.tocken) {
         const number = await request.getStatusNumber(req.body.number);
         if (number) {
             res.send({status: 200, statusNumber : number.data.statusNumber, number: number.data.number})
-        // }
+        }
     }
 });
 
