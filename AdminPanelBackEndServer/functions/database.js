be9562f54c1ad339a353ec247ab4c1fe9513193d
Admin <dzezelomihail@gmail.com>
2020-12-21 14:35:28 +0200

    add assets

--- AdminPanelBackEndServer/functions/database.js
+++ AdminPanelBackEndServer/functions/database.js
@@ -2,6 +2,7 @@ const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 const assets = require('../serverData.json');
 const {lofInfo, lofError} = require('./logger');
+const baseValue = require('../assets/defaultValue/value.json');
 
 class  Database {
     constructor() {
@@ -20,12 +21,11 @@ class  Database {
             reconnectTries: 60,
             // wait 1 second before retrying
             reconnectInterval: 1000
-        }).then(client => {
+        }).then(async client => {
             lofInfo("connect to base");
             console.log('connect')
              this.db = client.db(this.dbName);
-             this.userCollection = this.db.collection("users");
-             this.numberCollection = this.db.collection("numberstatus");
+             await this.createBase()
         }).catch(err => {
             lofError("error connect to base " + JSON.stringify(err));
             assert.equal(null, err);
@@ -33,14 +33,53 @@ class  Database {
         });
     }
 
+    async createBase() {
+                console.log(await this.db.listCollections().toArray());
+        const collections = await this.db.listCollections().toArray();
+            if (!collections.filter(item => {
+                return item.name === 'users';
+            })) {
+                await this.db.createCollection("users");
+            }
+        if (!collections.filter(item => {
+            return item.name === 'numberstatus';
+        })) {
+            await this.db.createCollection("numberstatus");
+        }
+
+        this.userCollection = this.db.collection("users");
+        this.numberCollection = this.db.collection("numberstatus");
+         await this.checkPerson();
+    }
+
     async findUser(userData){
         lofInfo("find user from base" + userData);
-        return  await  this.userCollection.findOne(userData);
+        try {
+            return await this.userCollection.findOne(userData);
+        }catch (e) {
+            console.log(e);
+        }
     }
 
     async getNumberStatus(number){
         lofInfo("find number from base" + number);
         return  await  this.numberCollection.findOne(number);
+    }
+
+    async checkPerson() {
+        const users = await this.userCollection.find({}).toArray();
+        console.log(users.length)
+        if (!users.length) {
+            await this.addValueToBase();
+        }
+    }
+
+    async addValueToBase() {
+        for (let key in baseValue) {
+            for (let i = 0; i < baseValue[key].length; i++) {
+                await this.db.collection(key).insertOne(baseValue[key][i])
+            }
+        }
     }
 
 }
---
+++ AdminPanelFrontServer/build/precache-manifest.abbfa7f99ff056dd99cf3d02539bf539.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d903176ace97f01f26840c4d9b754e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8367a30997b3e827d705",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "11aa8d8423b9417d0544",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "8367a30997b3e827d705",
+    "url": "/static/js/2.63d22e20.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.63d22e20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11aa8d8423b9417d0544",
+    "url": "/static/js/main.96d0a48f.chunk.js"
+  },
+  {
+    "revision": "6bf74636bb4e2e358201",
+    "url": "/static/js/runtime-main.32e3b4e5.js"
+  }
+]);
